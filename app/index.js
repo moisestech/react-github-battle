89b289350ece2771d802ef650d00180eb4bf9e04
Moises Sanabria <sesmoi.data@gmail.com>
2020-08-15 08:45:34 -0400

    code splitting route level components using React.lazy

--- app/index.js
+++ app/index.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-
-import Popular from './components/Popular/Popular'
-import Battle from './components/Battle/Battle'
-import Results from './components/Results/Results'
 import { ThemeProvider } from './contexts/theme'
 import Nav from './components/Nav/Nav'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Loading from './components/Loading/Loading'
+
+const Popular = React.lazy(() => import('./components/Popular/Popular'))
+const Battle = React.lazy(() => import('./components/Battle/Battle'))
+const Results = React.lazy(() => import('./components/Results/Results'))
 
 // Component
 // State
@@ -31,12 +32,14 @@ class App extends React.Component {
             <div className='container'>
               <Nav />
 
+          <React.Suspense fallback={<Loading />} >
             <Switch>
               <Route exact path='/' component={Popular} />
               <Route exact path='/battle' component={Battle} />
               <Route path='/battle/results' component={Results} />
               <Route render={() => <h1>404</h1>} />
             </Switch>
+          </React.Suspense>
             </div>
           </div>
         </ThemeProvider>
