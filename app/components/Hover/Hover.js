e3d86bfd4ba0c2c8025731512f27c13c202698ce
Moises Sanabria <sesmoi.data@gmail.com>
2020-08-15 07:40:32 -0400

    save-dev plugins class properties and refactores class components

--- app/components/Hover/Hover.js
+++ app/components/Hover/Hover.js
@@ -2,26 +2,11 @@ import React from 'react'
 
 
 export default class Hover extends React.Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      hovering: false
-    }
-
-    this.mouseOver = this.mouseOver.bind(this)
-    this.mouseOut = this.mouseOut.bind(this)
+  state = {
+    hovering: false
   }
-  mouseOver() {
-    this.setState({
-      hovering: true
-    })
-  }
-  mouseOut() {
-    this.setState({
-      hovering: false
-    })
-  }
+  mouseOver = () => this.setState({ hovering: true })
+  mouseOut = () => this.setState({ hovering: false })
   render () {
     return (
       <div
--- app/index.js
+++ app/index.js
@@ -7,7 +7,7 @@ import Battle from './components/Battle/Battle'
 import Results from './components/Results/Results'
 import { ThemeProvider } from './contexts/theme'
 import Nav from './components/Nav/Nav'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 // Component
 // State
@@ -15,16 +15,12 @@ import { BrowserRouter as Router, Route } from 'react-router-dom'
 // UI
 
 class App extends React.Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      theme: 'light',
-      toggleTheme: () => {
-        this.setState(({ theme }) => ({
-          theme: theme === 'light' ? 'dark' : 'light'
-        }))
-      }
+  state = {
+    theme: 'light',
+    toggleTheme: () => {
+      this.setState(({ theme }) => ({
+        theme: theme === 'light' ? 'dark' : 'light'
+      }))
     }
   }
   render () {
@@ -35,9 +31,12 @@ class App extends React.Component {
             <div className='container'>
               <Nav />
 
+            <Switch>
               <Route exact path='/' component={Popular} />
               <Route exact path='/battle' component={Battle} />
               <Route path='/battle/results' component={Results} />
+              <Route render={() => <h1>404</h1>} />
+            </Switch>
             </div>
           </div>
         </ThemeProvider>
