add537d5b0e83cb2b95c0399d4b90b2d9adb2e73
Moises Sanabria <sesmoi.data@gmail.com>
2020-10-24 06:39:18 -0400

    destructured useState, useEffect, useReducer, useRef

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,42 +1,54 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { fetchPopularRepos } from '../utils/api'
-import { FaUser, FaStar, FaCodeBranch, FaExclamationTriangle } from 'react-icons/fa'
-import Card from './Card'
-import Loading from './Loading'
-import Tooltip from './Tooltip'
+import { useState, useEffect, useReducer, useRef } from "react";
+import PropTypes from "prop-types";
+import { fetchPopularRepos } from "../utils/api";
+import {
+  FaUser,
+  FaStar,
+  FaCodeBranch,
+  FaExclamationTriangle,
+} from "react-icons/fa";
+import Card from "./Card";
+import Loading from "./Loading";
+import Tooltip from "./Tooltip";
 
-function LanguagesNav ({ selected, onUpdateLanguage }) {
-  const languages = ['All', 'Javascript', 'Ruby', 'Java', 'CSS', 'Python']
+function LanguagesNav({ selected, onUpdateLanguage }) {
+  const languages = ["All", "Javascript", "Ruby", "Java", "CSS", "Python"];
 
   return (
-    <ul className='flex-center'>
+    <ul className="flex-center">
       {languages.map((language) => (
         <li key={language}>
           <button
-            className='btn-clear nav-link'
-            style = {language === selected ? {color: 'rgb(187, 46,31)'} : null}
-            onClick={() => onUpdateLanguage(language)}>
+            className="btn-clear nav-link"
+            style={language === selected ? { color: "rgb(187, 46,31)" } : null}
+            onClick={() => onUpdateLanguage(language)}
+          >
             {language}
           </button>
         </li>
       ))}
     </ul>
-  )
+  );
 }
 
 LanguagesNav.propTypes = {
   selected: PropTypes.string.isRequired,
-  onUpdateLanguage: PropTypes.func.isRequired
-}
+  onUpdateLanguage: PropTypes.func.isRequired,
+};
 
-
-function ReposGrid ({ repos }) {
+function ReposGrid({ repos }) {
   return (
-    <ul className='grid space-around'>
+    <ul className="grid space-around">
       {repos.map((repo, index) => {
-        const { name, owner, html_url, stargazers_count, forks, open_issues } = repo
-        const { login, avatar_url } = owner
+        const {
+          name,
+          owner,
+          html_url,
+          stargazers_count,
+          forks,
+          open_issues,
+        } = repo;
+        const { login, avatar_url } = owner;
 
         return (
           <li key={html_url}>
@@ -46,80 +58,72 @@ function ReposGrid ({ repos }) {
               href={html_url}
               name={login}
             >
-              <ul className='card-list'>
+              <ul className="card-list">
                 <li>
                   <Tooltip text="Github username">
-                    <FaUser color='rgb(255, 191, 116)' size={22} />
-                    <a href={`https://github.com/${login}`}>
-                      {login}
-                    </a>
+                    <FaUser color="rgb(255, 191, 116)" size={22} />
+                    <a href={`https://github.com/${login}`}>{login}</a>
                   </Tooltip>
                 </li>
                 <li>
-                  <FaStar color='rgb(255, 215, 0)' size={22} />
+                  <FaStar color="rgb(255, 215, 0)" size={22} />
                   {stargazers_count.toLocaleString()} stars
                 </li>
                 <li>
-                  <FaCodeBranch color='rgb(129, 195, 245)' size={22} />
+                  <FaCodeBranch color="rgb(129, 195, 245)" size={22} />
                   {forks.toLocaleString()} forks
                 </li>
                 <li>
-                  <FaExclamationTriangle color='rgb(241, 138, 147)' size={22} />
+                  <FaExclamationTriangle color="rgb(241, 138, 147)" size={22} />
                   {open_issues.toLocaleString()} open
                 </li>
               </ul>
             </Card>
           </li>
-        )
+        );
       })}
     </ul>
-  )
+  );
 }
 
 ReposGrid.propTypes = {
-  repos: PropTypes.array.isRequired
+  repos: PropTypes.array.isRequired,
+};
 
-}
-
-function popularReducer (state, action) {
-  if (action.type === 'success') {
+function popularReducer(state, action) {
+  if (action.type === "success") {
     return {
       ...state,
       [action.selectedLanguage]: action.repos,
-      error: null
-    }
-  } else if (action.type === 'error') {
+      error: null,
+    };
+  } else if (action.type === "error") {
     return {
       ...state,
-      error: action.error.message
-    }
+      error: action.error.message,
+    };
   } else {
-    throw new Error(`'That action type isn't supporter.`)
+    throw new Error(`'That action type isn't supported.`);
   }
 }
 
-export default function Popular () {
-  const [selectedLanguage, setSelectedLanguage] = React.useState('All')
-  const [state, dispatch] = React.useReducer(
-    popularReducer,
-    { error: null }
-  )
+export default function Popular() {
+  const [selectedLanguage, setSelectedLanguage] = useState("All");
+  const [state, dispatch] = useReducer(popularReducer, { error: null });
 
+  const fetchedLanguages = useRef([]);
 
-  const fetchedLanguages = React.useRef([])
-
-
-  React.useEffect(() => {
+  useEffect(() => {
     if (fetchedLanguages.current.includes(selectedLanguage) === false) {
-      fetchedLanguages.current.push(selectedLanguage)
+      fetchedLanguages.current.push(selectedLanguage);
 
       fetchPopularRepos(selectedLanguage)
-        .then((repos) => dispatch({ type: 'success', selectedLanguage, repos}))
-        .catch((error) => dispatch({ type: 'error', error }))
+        .then((repos) => dispatch({ type: "success", selectedLanguage, repos }))
+        .catch((error) => dispatch({ type: "error", error }));
     }
-  }, [fetchedLanguages, selectedLanguage])
+  }, [fetchedLanguages, selectedLanguage]);
 
-  const isLoading = () => !state[selectedLanguage] && state.error === null
+  const isLoading = () => !state[selectedLanguage] && state.error === null;
 
   return (
     <>
@@ -128,11 +132,11 @@ export default function Popular () {
         onUpdateLanguage={setSelectedLanguage}
       />
 
-      {isLoading() && <Loading text='Fetching Repos' />}
+      {isLoading() && <Loading text="Fetching Repos" />}
 
-      {state.error && <p className='center-text error'>{state.error}</p>}
+      {state.error && <p className="center-text error">{state.error}</p>}
 
       {state[selectedLanguage] && <ReposGrid repos={state[selectedLanguage]} />}
     </>
-  )
+  );
 }
