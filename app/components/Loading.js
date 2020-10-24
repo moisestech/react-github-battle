903c84f6f11cca38ed21aec01a8ead7524435f4f
Moises Sanabria <sesmoi.data@gmail.com>
2020-10-24 06:38:01 -0400

    destructure useState, useEffect

--- app/components/Loading.js
+++ app/components/Loading.js
@@ -1,40 +1,34 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import { useState, useEffect } from "react";
+import PropTypes from "prop-types";
 
 const styles = {
   content: {
-    fontSize: '35px',
-    position: 'absolute',
-    left: '0',
-    right: '0',
-    marginTop: '20px',
-    textAlign: 'center',
-  }
-}
+    fontSize: "35px",
+    position: "absolute",
+    left: "0",
+    right: "0",
+    marginTop: "20px",
+    textAlign: "center",
+  },
+};
 
-export default function Loading ({ text = 'Loading', speed = 300}) {
-  const [content, setContent] = React.useState(text)
+export default function Loading({ text = "Loading", speed = 300 }) {
+  const [content, setContent] = useState(text);
 
-  React.useEffect(() => {
+  useEffect(() => {
     const id = window.setInterval(() => {
       setContent((content) => {
-        return content === `${text}...`
-        ? text
-        : `${content}.`
-      })
-    }, speed)
+        return content === `${text}...` ? text : `${content}.`;
+      });
+    }, speed);
 
-    return () => window.clearInterval(id)
-  }, [text, speed])
+    return () => window.clearInterval(id);
+  }, [text, speed]);
 
-  return (
-    <p style={styles.content}>
-      {content}
-    </p>
-  )
+  return <p style={styles.content}>{content}</p>;
 }
 
 Loading.propTypes = {
   text: PropTypes.string,
-  speed: PropTypes.number
-}
+  speed: PropTypes.number,
+};
