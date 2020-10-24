39e93b06a063c7ec1b90331d25b94bfba88f9e35
Moises Sanabria <sesmoi.data@gmail.com>
2020-10-24 06:41:00 -0400

    Prettier support

--- app/components/Tooltip.js
+++ app/components/Tooltip.js
@@ -1,43 +1,39 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import useHover from '../hooks/useHover'
+import PropTypes from "prop-types";
+import useHover from "../hooks/useHover";
 
 const styles = {
   container: {
-    position: 'relative',
-    display: 'flex'
+    position: "relative",
+    display: "flex",
   },
   tooltip: {
-    boxSizing: 'border-box',
-    position: 'absolute',
-    width: '160px',
-    bottom: '100%',
-    left: '50%',
-    marginLeft: '-80px',
-    borderRadius: '3px',
-    backgroundColor: 'hsla(0, 0%, 20%, 0.9)',
-    padding: '7px',
-    marginBottom: '5px',
-    color: '#fff',
-    textAlign: 'center',
-    fontSize: '14px'
-  }
-}
+    boxSizing: "border-box",
+    position: "absolute",
+    width: "160px",
+    bottom: "100%",
+    left: "50%",
+    marginLeft: "-80px",
+    borderRadius: "3px",
+    backgroundColor: "hsla(0, 0%, 20%, 0.9)",
+    padding: "7px",
+    marginBottom: "5px",
+    color: "#fff",
+    textAlign: "center",
+    fontSize: "14px",
+  },
+};
 
-
-export default function Tooltip ({ text, children }) {
-  const [hovering, attrs] = useHover()
+export default function Tooltip({ text, children }) {
+  const [hovering, attrs] = useHover();
 
   return (
-    <div  style={styles.container} {...attrs}>
-      {hovering === true && <div style={styles.tooltip}>
-        {text}</div>}
+    <div style={styles.container} {...attrs}>
+      {hovering === true && <div style={styles.tooltip}>{text}</div>}
       {children}
     </div>
-  )
+  );
 }
 
 Tooltip.propTypes = {
-  text: PropTypes.string.isRequired
-}
-
+  text: PropTypes.string.isRequired,
+};
