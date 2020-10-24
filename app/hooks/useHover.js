a18929188e0b8f6c9238ab2dc25f8ad94fb8f1b8
Moises Sanabria <sesmoi.data@gmail.com>
2020-10-24 06:41:56 -0400

    destructure useState

--- app/hooks/useHover.js
+++ app/hooks/useHover.js
@@ -1,13 +1,16 @@
-import React from 'react'
+import { useState } from "react";
 
-export default function useHover () {
-  const [hovering, setHovering] = React.useState(false)
+export default function useHover() {
+  const [hovering, setHovering] = useState(false);
 
-  const onMouseOver = () => setHovering(true)
-  const onMouseOut = () => setHovering(false)
+  const onMouseOver = () => setHovering(true);
+  const onMouseOut = () => setHovering(false);
 
-  return [hovering, {
-    onMouseOut,
-    onMouseOver
-  }]
+  return [
+    hovering,
+    {
+      onMouseOut,
+      onMouseOver,
+    },
+  ];
 }
