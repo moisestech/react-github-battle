12236a98edb064e582b001aa04087a3192555dcf
Moises Sanabria <sesmoi.data@gmail.com>
2020-10-31 15:14:18 -0400

    refactored Loading into folder for modularization

---
+++ src/components/Loading/index.js
@@ -0,0 +1,34 @@
+import { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+
+const styles = {
+  content: {
+    fontSize: "35px",
+    position: "absolute",
+    left: "0",
+    right: "0",
+    marginTop: "20px",
+    textAlign: "center",
+  },
+};
+
+export default function Loading({ text = "Loading", speed = 300 }) {
+  const [content, setContent] = useState(text);
+
+  useEffect(() => {
+    const id = window.setInterval(() => {
+      setContent((content) => {
+        return content === `${text}...` ? text : `${content}.`;
+      });
+    }, speed);
+
+    return () => window.clearInterval(id);
+  }, [text, speed]);
+
+  return <p style={styles.content}>{content}</p>;
+}
+
+Loading.propTypes = {
+  text: PropTypes.string,
+  speed: PropTypes.number,
+};
