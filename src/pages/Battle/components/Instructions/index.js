c0eb9e43e3a9983d59a5b3bb247e54431767e9dc
Moises Sanabria <sesmoi.data@gmail.com>
2020-11-01 05:16:47 -0500

    refactor into src, new func comp to refactor Battle Page

---
+++ src/pages/Battle/components/Instructions/index.js
@@ -0,0 +1,40 @@
+import "./instructions.css";
+
+// CONTEXT
+import ThemeContext from "../../../../contexts/theme";
+import { useContext } from "react";
+
+// ICONS
+import { FaUserFriends, FaFighterJet, FaTrophy } from "react-icons/fa";
+
+export default function Instructions() {
+  const theme = useContext(ThemeContext);
+
+  return (
+    <div className="instructions-container">
+      <h1 className="center-text header-lg">Instructions</h1>
+      <ol className="container-sm grid center-text battle-instructions">
+        <li>
+          <h3 className="header-sm">Enter two Github users</h3>
+          <FaUserFriends
+            className={`bg-${theme}`}
+            color="rgb(255, 191, 116)"
+            size={140}
+          />
+        </li>
+        <li>
+          <h3 className="header-sm">Battle</h3>
+          <FaFighterJet className={`bg-${theme}`} color="#727272" size={140} />
+        </li>
+        <li>
+          <h3 className="header-sm">See the winners</h3>
+          <FaTrophy
+            className={`bg-${theme}`}
+            color="rgb(255, 215, 0)"
+            size={140}
+          />
+        </li>
+      </ol>
+    </div>
+  );
+}
