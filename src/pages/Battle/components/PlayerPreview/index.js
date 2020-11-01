4071d2a52dcd599dbb217f311b3f7efa4eda39a2
Moises Sanabria <sesmoi.data@gmail.com>
2020-11-01 05:22:30 -0500

    refactored func comp from Battle Page impo ThemeContext and player-preview.css

---
+++ src/pages/Battle/components/PlayerPreview/index.js
@@ -0,0 +1,41 @@
+import "./player-preview.css";
+import { useState } from "react";
+import PropTypes from "prop-types";
+
+// CONTEXT
+import ThemeContext from "../../../../contexts/theme";
+import { useContext } from "react";
+
+// ICONS
+import { FaTimesCircle } from "react-icons/fa";
+
+export default function PlayerPreview({ username, onReset, label }) {
+  const theme = useContext(ThemeContext);
+
+  return (
+    <div className="column player">
+      <h3 className="player-label">{label}</h3>
+      <div className={`row bg-${theme}`}>
+        <div className="player-info">
+          <img
+            className="avatar-small"
+            src={`https://github.com/${username}.png?size=200`}
+            alt={`Avatar for ${username}`}
+          />
+          <a href={`https://github.com/${username}`} className="link">
+            {username}
+          </a>
+        </div>
+        <button className="btn-clear flex-center" onClick={onReset}>
+          <FaTimesCircle color="rgb(194, 57, 42)" size={26} />
+        </button>
+      </div>
+    </div>
+  );
+}
+
+PlayerPreview.propTypes = {
+  username: PropTypes.string.isRequired,
+  onReset: PropTypes.func.isRequired,
+  label: PropTypes.string.isRequired,
+};
